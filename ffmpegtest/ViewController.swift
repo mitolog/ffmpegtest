81c97a02e87ddfab418e8b09eaef39b6f68f34ac
mito <hkyhm5l@gmail.com>
2015-04-17 14:39:04 +0900

    Initial Commit

---
+++ ffmpegtest/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ffmpegtest
+//
+//  Created by YuheiMiyazato on 4/17/15.
+//  Copyright (c) 2015 mitolab. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
